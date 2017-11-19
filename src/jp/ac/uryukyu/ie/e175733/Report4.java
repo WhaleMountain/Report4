6922e0eda6c8c990001a299edd843ec184ee06e1
Ken Miyahira <e175733@ie.u-ryukyu.ac.jp>
2017-11-19 11:51:58 +0900

    Step1:コード準備

--- src/jp/ac/uryukyu/ie/e175733/Report4.java
+++ src/jp/ac/uryukyu/ie/e175733/Report4.java
@@ -2,6 +2,7 @@ package jp.ac.uryukyu.ie.e175733;
 
 public class Report4 {
     public static void main(String[] args){
-        System.out.println("Hello,World!!");
+        String str=null;
+        System.out.println(str.length());
     }
 }
